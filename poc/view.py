3f2d6264bf2abc80922a77757632a24825f8ef7e
Marcos Dantas <marcos.adantas@hotmail.com>
2024-03-26 16:47:59 -0300

    poc update

--- poc/view.py
+++ poc/view.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from time import sleep
+from random import randint
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    sleep_time = randint(0, 3)
+    sleep(sleep_time)
+    return HttpResponse(f"I sleept for {sleep_time}")
