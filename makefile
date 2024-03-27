aea48904192a1148048ee520c0b72bdca8db2d9c
Marcos Dantas <marcos.adantas@hotmail.com>
2024-03-27 11:45:26 -0300

    poc update

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ run.django:
 	python manage.py runserver
 
 run.gunicorn:
-	gunicorn poc.wsgi:application --bind=0.0.0.0:8000 --timeout=300 --log-level info --workers=8 --threads=4 --max-requests=500
+	gunicorn poc.wsgi:application --bind=0.0.0.0:8000 --timeout=300 --log-level info --workers=1 --threads=1 --max-requests=500
 
 run.gunicorn.uvicorn:
 	gunicorn poc.wsgi:application --bind=0.0.0.0:8000 --timeout=300 --log-level info --workers=12 --max-requests=500 -k uvicorn.workers.UvicornWorker
--- poc/view.py
+++ poc/view.py
@@ -1,28 +1,37 @@
+from typing import Any
 from django.http import HttpRequest, HttpResponse
 from time import sleep
 from random import uniform
 from asyncio import sleep as asleep
 from .session import Session
 from django.views import View
+from asgiref.sync import async_to_sync, sync_to_async
 
 class AsyncView(View):
-    session = Session()
-    def get(self, request, *args, **kwargs):
+    session: Session
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.session = Session()
+
+    @async_to_sync()
+    async def get(self, request, *args, **kwargs):
         user_email = request.GET.get('email')
         user_email = f"{user_email}@django-poc.com"
         print(f"email {user_email}")
         self.session.set_email(user_email)
         io_time = float(request.GET.get('sleep', 3.9))
         sleep_time = uniform(0, io_time)
-        # await asleep(sleep_time)
-        print(f"This is my session {self.session}")
+        await asleep(sleep_time)
         return HttpResponse(f"I napped for {sleep_time} seconds - user {self.session.get_email()}")
 
 def sync_sleep(request):
-    sleep_time = uniform(0, 4.9)
+    user_email = request.GET.get('email')
+    user_email = f"{user_email}@django-poc.com"
+    io_time = float(request.GET.get('sleep', 3.9))
+    sleep_time = uniform(0, io_time)
     sleep(sleep_time)
     return HttpResponse(f"I napped for {sleep_time} seconds")
-
 
 async def async_sleep(request: HttpRequest):
     session = Session()
